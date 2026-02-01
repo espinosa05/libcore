bf6e20bf6f210e5335ba319bfaa3c488d8e50b31
espinosa05 <francisco-fischer@gmx.de>
2026-02-01 15:32:45 +0100

    moved render_test to libkiek

--- test/render_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-
-gcc -c render_test.c -I ../include/ -I ../../libkiek/include/
-gcc render_test.o ../../libkiek/kiek/out/libkiek.a ../core/out/libcore.a -o render_test -lvulkan -lxcb -lxcb-icccm -lxcb-keysyms
-./render_test
